a8e66d5e5893434c00be3f2fa8ccd03cfae6fd2a
Leah Velleman <lbv@stripe.com>
2022-12-27 13:26:23 -0500

    Word accepts kwargs and requires a definition

--- src/word.py
+++ src/word.py
@@ -1,8 +1,21 @@
 from copy import deepcopy
+from src.root import Root
+from src.affix import Affix
 
 class Word():
-    def __init__(self, morphemes=None):
-        self.morphemes = morphemes or []
+    def __init__(self, definition, morphemes=None, **kwargs):
+        self.kind = "word"
+        self.definition = definition
+        self.initialize_morphemes(morphemes, **kwargs)
+
+    def initialize_morphemes(self, morphemes, **kwargs):
+        if morphemes is not None:
+            if all([type(m) in [Root, Affix] for m in morphemes]):
+                self.morphemes = morphemes
+            else:
+                raise Exception
+        else:
+            self.morphemes = [Root(**kwargs)]
 
     def affix(self, a):
         if a.side == "left":
--- tests/test_src/test_word.py
+++ tests/test_src/test_word.py
@@ -1,42 +1,50 @@
+import pytest
+
 from src.word import Word
 from src.root import Root
 from src.affix import Affix
 from src.inflection import Inflection
 from src.sounds import Sounds
 
+def test_words_require_definitions():
+    with pytest.raises(Exception):
+        w = Word(morphemes=[])
+
 def test_left_affixing_with_segments_works():
-    segs = [Sounds("ch", "tʃ")]
-    a = Affix(side="left", segments=segs, gloss="test")
-    r = Root(segments=[Sounds("a", "a")], gloss="gloss")
-    w = Word([r])
+    a = Affix(side="left", spelling="ch", ipa="ch", gloss="test")
+    r = Root(spelling="a", ipa="a", gloss="gloss")
+    w = Word(definition="foo", morphemes=[r])
     w.affix(a)
-    assert w.morphemes[0].segments == segs
+    assert w.morphemes[0].segments[0].spelling == "ch"
 
 def test_right_affixing_with_segments_works():
-    segs = [Sounds("ch", "tʃ")]
-    a = Affix(side="right", segments=segs, gloss="test")
-    r = Root(segments=[Sounds("a", "a")], gloss="gloss")
-    w = Word([r])
+    a = Affix(side="right", spelling="ch", ipa="ch", gloss="test")
+    r = Root(spelling="a", ipa="a", gloss="gloss")
+    w = Word(definition="foo", morphemes=[r])
     w.affix(a)
-    assert w.morphemes[-1].segments == segs
+    assert w.morphemes[-1].segments[0].spelling == "ch"
+
+def test_create_word_from_kwargs():
+    w = Word(definition="a", spelling="b", ipa="c", gloss="d")
+    assert w.morphemes[0].segments[0].ipa == "c"
 
 def test_you_can_apply_an_inflection():
-    w = Word([])
+    w = Word(spelling="a", ipa="b", gloss="c", definition="d")
     i = Inflection(side="left", gloss="test")
     assert w.apply(i)
 
 def test_applying_an_inflection_creates_an_affix():
-    w = Word([])
+    w = Word(spelling="a", ipa="b", gloss="c", definition="d")
     i = Inflection(side="left", gloss="test")
     w2 = w.apply(i)
     assert type(w2.morphemes[0]) == Affix
 
 def test_applying_an_inflection_leaves_original_word_unchanged():
-    w = Word([])
-    assert len(w.morphemes) == 0
+    w = Word(spelling="a", ipa="b", gloss="c", definition="d")
+    assert len(w.morphemes) == 1
     i = Inflection(side="left", gloss="test")
     w.apply(i)
-    assert len(w.morphemes) == 0
+    assert len(w.morphemes) == 1
 
 def test_subclassing():
     dep_suffix = Inflection(side="right", gloss="test", segments=[Sounds(ipa="h")])
@@ -44,7 +52,7 @@ def test_subclassing():
         def dep(self):
             return self.apply(dep_suffix)
     
-    w = Noun()
+    w = Noun(spelling="a", ipa="b", gloss="c", definition="d")
     assert w.dep().morphemes[-1].segments[0].ipa == "h"
 
 def test_subclassing_with_function():
@@ -57,7 +65,7 @@ def test_subclassing_with_function():
         def dep(self):
             return self.apply(dep_suffix)
     
-    w = Noun([Root(Sounds(ipa="kora"))])
+    w = Noun(spelling="a", ipa="b", gloss="c", definition="d")
     assert w.dep().morphemes[-1].segments[0].ipa == "h"
 
 
